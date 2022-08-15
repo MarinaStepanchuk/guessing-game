ff5d03e2f2b81515f8748b513b411d72064ed973
MarinaStepanchuk <mari20002@mail.ru>
2022-08-15 23:42:18 +0300

    add: result

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.num = 0;
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.num = Math.ceil((this.max - this.min) / 2 + this.min);
     }
 
     lower() {
-
+        this.max = this.num;
     }
 
     greater() {
-
+        this.min = this.num;
     }
 }
 
